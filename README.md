c5395bddefcd4c0e523ac1c5ebc36fd16eec2fc5
Vanessa <vwallis@gmail.com>
2013-04-22 23:35:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Make Awesome - Sandwich Edition
+
+This is a Making Awesome Enginerding Learning Funscursion, affectionately and enthusiastically brought to you by
+[*creatigeek*](http://creatigeek.com/) aka [Vanessa P Wallis](http://www.linkedin.com/in/vanessapwallis).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeSandwich::Application.config.secret_token = 'f594d6b20ec0d0d9cdc36ff0682dfaee1a5fa05d5d17d1fdb590c5dd1f1c85d9e45f1a4d563cca9ef10ded5a7177bb7ac7f007eac2d246c1f486338ceee1a8bd'
