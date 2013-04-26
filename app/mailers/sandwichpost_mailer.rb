07c98c6ab273df117b3df6ec7b57b312e7c0a72b
Vanessa <vwallis@gmail.com>
2013-04-25 21:22:35 -0700

    sandwichrequest_email to: and cc: fields for demo times. (:

--- app/mailers/sandwichpost_mailer.rb
+++ app/mailers/sandwichpost_mailer.rb
@@ -5,7 +5,7 @@ class SandwichpostMailer < ActionMailer::Base
   def sandwichrequest_email(user, sandwichpost)
     @user = user
     @sandwichpost = sandwichpost
-    mail(to: 'vwallis@gmail.com', subject: 'Plz to make a sandwich kthx (: <3')
+    mail(to: 'make_sandwich@generalthings.com', cc: 'vwallis@gmail.com', subject: 'Plz to make a sandwich kthx (: <3')
   end
     
 end
