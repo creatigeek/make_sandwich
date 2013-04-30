975c4f9ed71391f1864630d3b62fa44a5b8a198c
Vanessa <vwallis@gmail.com>
2013-04-29 21:10:21 -0700

    changes SandwichpostMailer to: and cc: fields, to application creator and user, respectively

--- app/mailers/sandwichpost_mailer.rb
+++ app/mailers/sandwichpost_mailer.rb
@@ -5,7 +5,7 @@ class SandwichpostMailer < ActionMailer::Base
   def sandwichrequest_email(user, sandwichpost)
     @user = user
     @sandwichpost = sandwichpost
-    mail(to: 'make_sandwich@generalthings.com', cc: 'vwallis@gmail.com', subject: 'Plz to make a sandwich kthx (: <3')
+    mail(to: 'vwallis@gmail.com', cc: @user.email, subject: 'Plz to make a sandwich kthx (: <3')
   end
     
 end
